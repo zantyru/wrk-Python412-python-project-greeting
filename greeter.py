d9ef50182382873c5bdb443422697e34bd848a25
none <none@none.com>
2025-05-22 22:02:24 +0300

    Добавляет возможность задавать текст приветствия

--- greeter.py
+++ greeter.py
@@ -1,3 +1,6 @@
 class Greeter:
+    def __init__(self, greeting_text="Привет, Мир"):
+        self.__greeting_text = str(greeting_text)
+
     def greet(self):
-        return "Привет, мир!"
+        return self.__greeting_text
